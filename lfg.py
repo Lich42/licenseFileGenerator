80585f1d78f816e3d6c4f7a708d7e51a90a40857
Lich42 <halobobby123@gmail.com>
2019-07-01 15:39:17 -0400

    Added 3 new licenses.

--- lfg.py
+++ lfg.py
@@ -7,13 +7,15 @@ while True:
 
 	if liceName == "LIST": 
 		#Lists all the licenses and available and their commands
+		#NOTE: The only reason I've used multiple "prints" is to keep the source good looking neat
 		print("\n-------------\n  LICENSES\n-------------\n")
 		print("LICENSE                    COMMAND\n")
-		print("General Public License v3 = 'gpl'\nApache License = 'al'\nBSD 2 = 'bsd2'\nBSD 3 = 'bsd3'\nMIT = 'mit'\nUnlicense = 'ul'\nMozilla Public License v2 = 'mpl'\n")
+		print("General Public License v3 = 'gpl'\nApache License = 'al'\nBSD 2 = 'bsd2'\nBSD 3 = 'bsd3'\nMIT = 'mit'\nUnlicense = 'ul'\nMozilla Public License v2 = 'mpl'")
+		print("Affero General Public License = 'agpl'\nLesser General Public License v3 = 'lgpl'\nEclipse Public License = 'epl'\n")
 
 		continue
 
-	elif liceName == 'gpl' or liceName == 'al' or liceName == 'bsd2' or liceName == 'bsd3' or liceName == 'ul' or liceName == 'mit' or liceName == 'mpl': 
+	elif liceName == 'gpl' or liceName == 'al' or liceName == 'bsd2' or liceName == 'bsd3' or liceName == 'ul' or liceName == 'mit' or liceName == 'mpl' or liceName == 'agpl' or liceName == 'lgpl' or liceName == 'epl':
 		repoPath = input('Please type the path to the repository: ') 
 		liceFile = 'https://raw.githubusercontent.com/Lich42/LFG_Licenses/master/' + liceName + '.txt' #Gets the url of the user's desired license
 		data = requests.get(liceFile)
